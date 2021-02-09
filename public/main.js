85bf9b93c53b572c77868a8e697a309fcd85a12c
Satomi Ichii <sichii@gmail.com>
2021-02-08 21:42:22 -0500

    feat: PUSH NORIFICATION!

--- public/main.js
+++ public/main.js
@@ -1,4 +1,4 @@
-export const signup = user => {
+export const pushSetting = (user, signUp) => {
   'use strict'
 
   const axios = require('axios')
@@ -26,6 +26,8 @@ export const signup = user => {
           subscribeUser()
         }
       })
+    } else if (!signUp) {
+      unsubscribeUser()
     }
   }
 
@@ -65,13 +67,34 @@ export const signup = user => {
     }
   }
 
+  function unsubscribeUser() {
+    let userSubsctiption
+    swRegistration.pushManager
+      .getSubscription()
+      .then(subscription => {
+        if (subscription) {
+          userSubsctiption = subscription
+          return subscription.unsubscribe()
+        }
+      })
+      .catch(err => {
+        console.log('Error unsubscribing', err)
+      })
+      .then(() => {
+        if (userSubsctiption) {
+          updateSubscriptionOnServer(user.id, userSubsctiption, null)
+        }
+        console.log('User is unsubscribed')
+        isSubscribed = false
+      })
+  }
+
   async function updateSubscriptionOnServer(
     userId,
     subscription,
     unsubscribe = 'No'
   ) {
     // Here's where you would send the subscription to the application server
-    // code below is just for demo purpose. We have to replace this with ajax request to our app surver.
 
     //if the action is subscribe. hit add route
     try {
@@ -80,16 +103,11 @@ export const signup = user => {
         console.log('subscription', subscription)
         console.log('userId', userId)
         console.log('unsibscribe', unsubscribe)
-        await axios.get(`/api/subscription/${userId}`, subscription)
-
-        // subscriptionJson.textContent = JSON.stringify(subscription)
-        // endpointURL.textContent = subscription.endpoint
-        // subAndEndpoint.style.display = 'block'
+        await axios.post(`/api/subscription/${userId}`, subscription)
 
         //if the action is unsubscribe. hit delete route
       } else {
-        // await axios.delete(`/api/subscription/`, subscription)
-        // subAndEndpoint.style.display = 'none'
+        await axios.delete(`/api/subscription/${userId}`)
         console.log('User is not subscribed')
       }
     } catch (error) {
@@ -135,229 +153,3 @@ export const signup = user => {
     console.warn('Push messaging is not supported')
   }
 }
-
-// export const app = () => {
-//   'use strict'
-
-//   let isSubscribed = false
-//   let swRegistration = null
-
-//   const notifyButton = document.querySelector('.js-notify-btn')
-//   const pushButton = document.querySelector('.js-push-btn')
-
-//   if (!('Notification' in window)) {
-//     console.log('Notifications not supported in this browser')
-//     return
-//   }
-
-//   Notification.requestPermission((status) => {
-//     console.log('Notification permission status:', status)
-//   })
-
-//   function displayNotification() {
-//     if (Notification.permission === 'granted') {
-//       navigator.serviceWorker.getRegistration().then((reg) => {
-//         const options = {
-//           body: 'First notification!',
-//           // tag: 'id1',
-//           icon: 'images/notification-flat.png',
-//           vibrate: [100, 50, 100],
-//           data: {
-//             dateOfArrival: Date.now(),
-//             primaryKey: 1,
-//           },
-//           actions: [
-//             {
-//               action: 'explore',
-//               title: 'Go to the site',
-//               icon: 'images/checkmark.png',
-//             },
-//             {
-//               action: 'close',
-//               title: 'Close the notification',
-//               icon: 'images/xmark.png',
-//             },
-//           ],
-//         }
-//         reg.showNotification('Hello world!', options)
-//       })
-//     }
-//   }
-
-//   function initializeUI() {
-//     pushButton.addEventListener('click', () => {
-//       pushButton.disabled = true
-//       if (isSubscribed) {
-//         unsubscribeUser()
-//       } else {
-//         subscribeUser()
-//       }
-//     })
-
-//     // Set the initial subscription value
-//     swRegistration.pushManager.getSubscription().then((subscription) => {
-//       isSubscribed = subscription !== null
-
-//       updateSubscriptionOnServer(subscription)
-
-//       if (isSubscribed) {
-//         console.log('User IS subscribed.')
-//       } else {
-//         console.log('User is NOT subscribed.')
-//       }
-
-//       updateBtn()
-//     })
-//   }
-
-//   const applicationServerPublicKey =
-//     'BI5Oqd_Wm2nvGdMq5Kg8MmyzbHWgmWirU9LNo2jvfyvaQlCvX88-Ts88N8t7MdEilq_IoRs9QezC01kBXsucMGU'
-
-//   // on our app, when user click the reminder page submit button, this happen.
-//   function subscribeUser() {
-//     Notification.requestPermission((status) => {
-//       console.log('Notification permission status:', status)
-//     })
-
-//     if (Notification.permission === 'granted') {
-//       console.log('this is called')
-//       const applicationServerKey = urlB64ToUint8Array(
-//         applicationServerPublicKey
-//       )
-//       swRegistration.pushManager
-//         .subscribe({
-//           userVisibleOnly: true,
-//           applicationServerKey: applicationServerKey,
-//         })
-//         .then((subscription) => {
-//           console.log('User is subscribed:', subscription)
-
-//           updateSubscriptionOnServer(subscription)
-
-//           isSubscribed = true
-
-//           updateBtn()
-//         })
-//         .catch((err) => {
-//           if (Notification.permission === 'denied') {
-//             console.warn('Permission for notifications was denied')
-//           } else {
-//             console.error('Failed to subscribe the user: ', err)
-//           }
-//           updateBtn()
-//         })
-//     }
-//   }
-
-//   function unsubscribeUser() {
-//     let userSubsctiption
-//     swRegistration.pushManager
-//       .getSubscription()
-//       .then((subscription) => {
-//         if (subscription) {
-//           userSubsctiption = subscription
-//           return subscription.unsubscribe()
-//         }
-//       })
-//       .catch((err) => {
-//         console.log('Error unsubscribing', err)
-//       })
-//       .then(() => {
-//         updateSubscriptionOnServer(userSubsctiption, null)
-
-//         console.log('User is unsubscribed')
-//         isSubscribed = false
-
-//         updateBtn()
-//       })
-//   }
-
-//   async function updateSubscriptionOnServer(subscription, unsubscribe = 'No') {
-//     // Here's where you would send the subscription to the application server
-//     // code below is just for demo purpose. We have to replace this with ajax request to our app surver.
-
-//     // const subscriptionJson = document.querySelector('.js-subscription-json')
-//     // const endpointURL = document.querySelector('.js-endpoint-url')
-//     // const subAndEndpoint = document.querySelector('.js-sub-endpoint')
-
-//     //if the action is subscribe. hit add route
-//     try {
-//       if (unsubscribe) {
-//         console.log('subscription', subscription)
-//         // await axios.get(`/api/subscription/`, subscription)
-//         // subscriptionJson.textContent = JSON.stringify(subscription)
-//         // endpointURL.textContent = subscription.endpoint
-//         // subAndEndpoint.style.display = 'block'
-
-//         //if the action is unsubscribe. hit delete route
-//       } else {
-//         // await axios.delete(`/api/subscription/`, subscription)
-//         // subAndEndpoint.style.display = 'none'
-//         console.log('User is not subscribed')
-//       }
-//     } catch (error) {
-//       console.log('Update Subscription failed', error)
-//     }
-//   }
-
-//   function updateBtn() {
-//     if (Notification.permission === 'denied') {
-//       pushButton.textContent = 'Push Messaging Blocked'
-//       pushButton.disabled = true
-//       updateSubscriptionOnServer(null)
-//       return
-//     }
-
-//     if (isSubscribed) {
-//       pushButton.textContent = 'Disable Push Messaging'
-//     } else {
-//       pushButton.textContent = 'Enable Push Messaging'
-//     }
-
-//     pushButton.disabled = false
-//   }
-
-//   function urlB64ToUint8Array(base64String) {
-//     const padding = '='.repeat((4 - (base64String.length % 4)) % 4)
-//     const base64 = (base64String + padding)
-//       .replace(/\-/g, '+')
-//       .replace(/_/g, '/')
-
-//     const rawData = window.atob(base64)
-//     const outputArray = new Uint8Array(rawData.length)
-
-//     for (let i = 0; i < rawData.length; ++i) {
-//       outputArray[i] = rawData.charCodeAt(i)
-//     }
-//     return outputArray
-//   }
-
-//   notifyButton.addEventListener('click', () => {
-//     displayNotification()
-//   })
-
-//   if ('serviceWorker' in navigator) {
-//     window.addEventListener('load', () => {
-//       console.log('Service Worker and Push is supported')
-
-//       navigator.serviceWorker
-//         .register('sw.js')
-//         .then((swReg) => {
-//           console.log(
-//             'ServiceWorker registration successful with scope: ',
-//             swReg.scope
-//           )
-//           swRegistration = swReg
-//           window.swRegistration = swReg
-
-//           initializeUI()
-//         })
-//         .catch((err) => {
-//           console.error('Service Worker Error', err)
-//         })
-//     })
-//   } else {
-//     console.warn('Push messaging is not supported')
-//     pushButton.textContent = 'Push Not Supported'
-//   }
-// }
--- public/node/main.js
+++ public/node/main.js
@@ -1,41 +1,47 @@
 const webPush = require('web-push')
+const axios = require('axios')
 
-const pushSubscription = {
-  endpoint:
-    'https://fcm.googleapis.com/fcm/send/ekMIKwJIUEM:APA91bFr7Gn5tC38Z5H5E0OhS89SFCAQxMnrZDBo-FtwuwVLB9l-I5WgKQ_LSb-P-JmzZVQWM7s9Mxgp9E5vP5vjs2ND4s9FlAkDSz00ZGnU-e2SPDdYM57QySaw1POkIgxdK9f1zz1j',
-  expirationTime: null,
-  keys: {
-    p256dh:
-      'BGTviQpIi2QoxkSelssd7pWakVgqP7n2jUWA9ftIeaVgK98BDyTOaZWgDrWnqw58WHDNgN6v4sttliJmXdUiArc',
-    auth: '0n7MgxsLoi4n0svoYIkplQ'
+async function sendPush() {
+  try {
+    let {data} = await axios.get(
+      '/api/subscription/?reminderType=waterReminder',
+      {
+        proxy: {
+          host: 'localhost',
+          port: 8080
+        }
+      }
+    )
+
+    if (data.length) {
+      data = data.map(sub => {
+        sub.keys = JSON.parse(sub.keys)
+        return sub
+      })
+    }
+
+    const vapidPublicKey =
+      'BI5Oqd_Wm2nvGdMq5Kg8MmyzbHWgmWirU9LNo2jvfyvaQlCvX88-Ts88N8t7MdEilq_IoRs9QezC01kBXsucMGU'
+    const vapidPrivateKey = 'JKqvzjBqcIHUQkHtu0Xon9fgjp-ob_ZeDSSePPXxL9A'
+
+    const payload = 'Time to drink water!!'
+
+    const options = {
+      // gcmAPIKey: 'YOUR_SERVER_KEY',
+      TTL: 60,
+      vapidDetails: {
+        subject: 'mailto:zitac234@gmail.com',
+        publicKey: vapidPublicKey,
+        privateKey: vapidPrivateKey
+      }
+    }
+
+    data.forEach(sub => {
+      webPush.sendNotification(sub, payload, options)
+    })
+  } catch (error) {
+    console.log(error)
   }
 }
 
-// {
-//   endpoint:
-//     'https://fcm.googleapis.com/fcm/send/d0cQRWP89W8:APA91bH2lwmd8Yz_ClJU-7ClUuioHxHXe9QuLG1w_bMaJPTNLSmixWcZ0-88M9n15rUmKOthSYU4_UuR2GegLra4uGOZJnA8djC2R4PmbLR14a8dVjWgR30wQrFRRaP3itknmifkAdbu',
-//   expirationTime: null,
-// keys: {
-//   p256dh:
-//     'BL6r_rm2oUREwCGLZrbtctE56YEuBasz0K7wY4KwuSYactRNlQbRitEK900so56yObrschK8O3jiMkglSDMkf0A',
-//   auth: 'vjj0hpGz3MtIq2kMxSmoKQ',
-//   },
-// }
-
-const vapidPublicKey =
-  'BI5Oqd_Wm2nvGdMq5Kg8MmyzbHWgmWirU9LNo2jvfyvaQlCvX88-Ts88N8t7MdEilq_IoRs9QezC01kBXsucMGU'
-const vapidPrivateKey = 'JKqvzjBqcIHUQkHtu0Xon9fgjp-ob_ZeDSSePPXxL9A'
-
-const payload = 'Time to drink water!!'
-
-const options = {
-  // gcmAPIKey: 'YOUR_SERVER_KEY',
-  TTL: 60,
-  vapidDetails: {
-    subject: 'mailto:zitac234@gmail.com',
-    publicKey: vapidPublicKey,
-    privateKey: vapidPrivateKey
-  }
-}
-
-webPush.sendNotification(pushSubscription, payload, options)
+sendPush()
