a0099cd14c37f29f3b61cb9be265288ca120cd82
Satomi Ichii <sichii@gmail.com>
2021-02-12 10:59:47 -0500

    add: unlockNewLevel thunk

--- client/store/unlock.js
+++ client/store/unlock.js
@@ -1,11 +1,19 @@
 import axios from 'axios'
 
 const GET_FEED = 'GET_FEED'
+const POST_FEED = 'POST_FEED'
 
 const getFeed = allFeed => {
   return {
     type: GET_FEED,
     allFeed
+  }
+}
+
+const postFeed = newFeed => {
+  return {
+    type: POST_FEED,
+    newFeed
   }
 }
 
@@ -20,11 +28,24 @@ export const fetchAllFeed = () => {
   }
 }
 
+export const unlockNewLevel = (userId, levelId) => {
+  return async dispatch => {
+    try {
+      const {data} = await axios.post(`/api/unlock/${userId}/${levelId}`)
+      dispatch(postFeed(data))
+    } catch (error) {
+      console.log('This is error in unlockNewLevel Thunk', error)
+    }
+  }
+}
+
 const initialState = []
 export default function unlock(state = initialState, action) {
   switch (action.type) {
     case GET_FEED:
       return action.allFeed
+    case POST_FEED:
+      return [...state, action.newFeed]
     default:
       return state
   }
