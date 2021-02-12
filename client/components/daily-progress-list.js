8052edbaaf22c683df1224d9389afbcea10f63ac
satomiichii <61665901+satomiichii@users.noreply.github.com>
2021-02-12 13:36:19 -0500

    Update daily-progress-list.js

--- client/components/daily-progress-list.js
+++ client/components/daily-progress-list.js
@@ -1,6 +1,6 @@
 import List from '@material-ui/core/List'
 import ListItem from '@material-ui/core/ListItem'
-// import ListItemText from '@material-ui/core/ListItemText'
+import ListItemText from '@material-ui/core/ListItemText'
 import ListItemAvatar from '@material-ui/core/ListItemAvatar'
 import Avatar from '@material-ui/core/Avatar'
 import Checkbox from '@material-ui/core/Checkbox'
