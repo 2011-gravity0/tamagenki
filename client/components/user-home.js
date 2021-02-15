c695577b29a46e1b5b3c7dc44bda2e2ab07fa19f
angiepmunoz <angiepmunoz@gmail.com>
2021-02-15 07:54:43 -0500

    added onclick function on share

--- client/components/user-home.js
+++ client/components/user-home.js
@@ -11,6 +11,7 @@ import {compose} from 'redux'
 import {fetchList, fetchUpdatedList} from '../store/dailyProgress'
 import {fetchUserHistory, updateUser} from '../store/user'
 import {fetchResp} from '../store/owlResponse'
+import {unlockNewLevel} from '../store/unlock'
 import {Howl, Howler} from 'howler'
 import Navbar from './navbar'
 import Grid from '@material-ui/core/Grid'
@@ -825,6 +826,17 @@ export class UserHome extends React.Component {
       sparkle: '/badges/sparkle.svg'
     }
 
+    const modalImagesParams = {
+      water: 'badges/water.svg',
+      meditation: 'badges/meditation.svg',
+      exercise: 'badges/movement.svg',
+      fruit: 'badges/fruit.svg',
+      vegetables: 'badges/veg.svg',
+      sleep: 'badges/sleep.svg',
+      relaxation: 'badges/relaxation.svg',
+      sparkle: 'badges/sparkle.svg'
+    }
+
     const body = (
       <Grid container>
         <div
@@ -851,7 +863,13 @@ export class UserHome extends React.Component {
 
               <h2 className={classes.modalTitle}>{modalTitles[modal]}</h2>
             </Button>
-            <Button>Share</Button>
+            <Button
+              onClick={() =>
+                this.props.addBadgeToFeed(this.props.userId, modalTitles[modal])
+              }
+            >
+              Share
+            </Button>
           </Grid>
         </div>
       </Grid>
@@ -1175,7 +1193,9 @@ const mapDispatch = dispatch => {
     updateList: (column, points) => dispatch(fetchUpdatedList(column, points)),
     getUserHistory: userId => dispatch(fetchUserHistory(userId)),
     nameBuddy: (userId, data) => dispatch(updateUser(userId, data)),
-    getOwlResp: () => dispatch(fetchResp())
+    getOwlResp: () => dispatch(fetchResp()),
+    addBadgeToFeed: (userId, levelId) =>
+      dispatch(unlockNewLevel(userId, levelId))
   }
 }
 
